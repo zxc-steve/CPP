8c97d4990cd06f6ac33f18359ccc38b470ef442f
zxc-steve <63087711+zxc-steve@users.noreply.github.com>
2025-09-29 22:37:12 +0000

    update all files

--- constructor.cpp
+++ constructor.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "constructor.h"
 using namespace std;
+ostream& operator<<(ostream& os,  const int* p){
+    os.operator<<(p)<<':';
+    for(int i=0;i<10;i++) os<<p[i]<<' ';
+    os<<endl;
+    return os;
+}
 class B{
 public:
     int d=1;
