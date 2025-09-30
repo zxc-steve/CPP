c14c0e6753c6791300ac5910025381087ac24ced
zxc-steve <63087711+zxc-steve@users.noreply.github.com>
2025-09-30 08:12:31 +0000

    update derived.cpp

--- derived.cpp
+++ derived.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+using namespace std;
+class B{
+  public:
+  int x=0;
+  int getx(){return x;}
+}
 int main(){
   std::cout<<"abc.cpp output";
   std::cout<<"test for code space"<<std::endl;
