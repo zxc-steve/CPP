76f113668113d2c91252709abcebf1f65d5b42b8
zxc-steve <63087711+zxc-steve@users.noreply.github.com>
2025-10-01 15:01:14 +0000

    add prime factor

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,10 +3,22 @@
 #include "lib_io.h"
 using namespace std;
 int main(){
-    cout<<"f1="<<f1()<<endl;
-    cout<<"f2="<<f2()<<endl;
-    cout<<"f3="<<f3()<<endl;
-    int a[10]={0,1,2,3,4,5,6,7,8,-99};
-    cout<<a;
-    return 0;   
+ int n=256;
+ int r=n,cnt=0;
+ while(r>1){
+     for(int i=2;i<=r;i++){
+         if(r%i == 0){
+             ++cnt;r=r/i;
+             cout <<'*'<<i;
+             break;
+         }
+     }
+}
+
+cout<<"f1="<<f1()<<endl;
+cout<<"f2="<<f2()<<endl;
+cout<<"f3="<<f3()<<endl;
+int a[10]={0,1,2,3,4,5,6,7,8,-99};
+cout<<a;
+return 0;   
 }
